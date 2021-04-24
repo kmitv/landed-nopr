431679ad4272bf8fec66bb74f9e81aa899947a14
kmitv <wojtek.kmita@hotmail.com>
2021-04-24 15:53:38 +0200

    fix svelte-check errors

--- src/components/MainCentral/MainCentral.svelte
+++ src/components/MainCentral/MainCentral.svelte
@@ -14,7 +14,7 @@
         return str;
     };
 
-    let interval;
+    let interval: NodeJS.Timeout;
     let randomString = random(500);
 
     onMount(async () => {
@@ -29,19 +29,19 @@
 </script>
 
 <div class="central-grid center-absolute">
-    <Tile rowStart="1" rowEnd="4" columnStart="1" columnEnd="4" >
+    <Tile rowStart="{1}" rowEnd="{4}" columnStart="{1}" columnEnd="{4}" >
         Coffee in old town Sarajevo, the historic site, where thousands of locals have come to enjoy the special drinks, the atmosphere, the beautiful old town buildings and the culture and history.
         The area has been a gathering place since the 19th century. There are many old buildings, many museums, several galleries, theaters, concerts, exhibitions and many other interesting things.
     </Tile>
-    <Tile rowStart="1" rowEnd="3" columnStart="4" columnEnd="6" >
+    <Tile rowStart="{1}" rowEnd="{3}" columnStart="{4}" columnEnd="{6}" >
         <div class="bordered-div">
             <div class="bordered-div">
                 <div class="bordered-div"></div>
             </div>
         </div>
     </Tile>
-    <Tile rowStart="3" rowEnd="3" columnStart="4" columnEnd="6" ><AccumulationPrecipitation32 /></Tile>
-    <Tile rowStart="4" rowEnd="6" columnStart="1" columnEnd="6" >
+    <Tile rowStart="{3}" rowEnd="{3}" columnStart="{4}" columnEnd="{6}" ><AccumulationPrecipitation32 /></Tile>
+    <Tile rowStart="{4}" rowEnd="{6}" columnStart="{1}" columnEnd="{6}" >
         {#if !!randomString}
             <div class="random-string-container">{randomString}</div>
         {/if}
